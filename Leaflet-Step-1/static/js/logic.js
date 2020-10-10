ee8f6d253d2652373f767b6f89b0cbe08e178bd6
Gianni Adamo <gianx1@yahoo.com>
2020-10-10 10:27:34 -0500

    final product polished

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -99,7 +99,7 @@ function createMap(earthquakes) {
     L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
       tileSize: 512,
-      maxZoom: 18,
+      maxZoom: 20,
       zoomOffset: -1,
       id: "mapbox/streets-v11",
       accessToken: API_KEY
